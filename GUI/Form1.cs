dc0dcfa7d1cf5b913e2c3e661b1870402cee1e13
Duarte Duarte <dnpd.dd@gmail.com>
2014-04-28 18:31:09 +0100

    Initial GUI project
    Implement method to draw a simple grid

---
+++ GUI/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var pen = new Pen(Color.Black);
+            e.Graphics.DrawGrid(pen, 0, 0, 500, 500, 10, 10);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
---
+++ GUI/GraphicsHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace GUI
+{
+    public static class GraphicsHelper
+    {
+        public static void DrawGrid(this Graphics g, Pen pen, float x, float y, float width, float height, int xDivisions, int yDivisions)
+        {
+            var xSize = width/xDivisions;
+            var ySize = height/yDivisions;
+
+            for (var xx = 0; xx <= xDivisions; ++xx)
+            {
+                g.DrawLine(pen, x + xx * xSize, y, x + xx * xSize, y + height);
+            }
+
+            for (var yy = 0; yy <= yDivisions; ++yy)
+            {
+                g.DrawLine(pen, x, y + yy * ySize, x + width, y + yy * ySize);
+            }
+        }
+    }
+}
